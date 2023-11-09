05ee8ac8fa719249da1152d55f9a3e2675086292
Gaetano <gaetanobarreca@gmail.com>
2023-11-08 21:34:07 -0800

    Working on chatbot button

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,26 @@
 
-function customOpenForm() {
+function toggleChat() {
   let form = document.getElementById("customMyForm");
-  let openButton = document.getElementById("custom-open-button");
-  if (form) {
+  let button = document.getElementById("custom-open-button");
+
+  if (form.style.display === "block") {
+    form.style.display = "none";
+    button.textContent = "Start Chat"; // Change button text to 'Start Chat'
+    // Optional: Reset the iframe when the chat is closed, if needed
+    resetIframeSrc();
+  } else {
     form.style.display = "block";
-  }
-  if (openButton) {
-    openButton.style.display = "none";
+    button.textContent = "End Chat"; // Change button text to 'End Chat'
+    // Call setIframeSrc to ensure the chatbot token is refreshed or iframe is loaded
+    setIframeSrc();
   }
 }
 
-function customCloseForm() {
-  let form = document.getElementById("customMyForm");
-  if (form) {
-    form.style.display = "none";
+// Optional: Function to reset the iframe source when the chat ends
+function resetIframeSrc() {
+  let iframe = document.getElementById("chatbot-iframe");
+  if (iframe) {
+    iframe.src = ""; // Clear the iframe source
   }
 }
 
@@ -37,6 +44,6 @@ async function setIframeSrc() {
   }
 }
 
-
-// Call setIframeSrc once the DOM is fully loaded
-document.addEventListener("DOMContentLoaded", setIframeSrc);
+// Call setIframeSrc once the DOM is fully loaded, if necessary
+// Commented out because setIframeSrc will be called when the chat opens
+// document.addEventListener("DOMContentLoaded", setIframeSrc);
